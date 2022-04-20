5fc035466dce125db6fbd1aab0cf8de07d3bf3e3
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2022-04-20 16:57:30 -0300

    Basic implementation of TaskForm

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,16 +1,39 @@
 import { Container } from "@mui/material"
+import { useState } from "react"
 import Header from "./layout/Header"
+import TaskForm from "./task/TaskForm"
 
 function MainPage() {
 
+    const [showTaskForm, setShowTaskForm] = useState(false)
+    const [task, setTask] = useState({})
+
     function handleNewTaskButton() {
         //toggle new task form
-        console.log("TODO toggle new task form")
+        setShowTaskForm(true)
+    }
+
+    function createTask(task) {
+        fetch('http://localhost:5000/tasks', {
+            method : "POST",
+            headers : {
+                'Content-type' : 'application/json'
+            },
+            body : JSON.stringify(task)
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log('Task criada!')
+        })
+        .catch((err) => console.log(err))
     }
 
     return (
         <Container maxWidth="md">
             <Header handleNewTaskButton={handleNewTaskButton} />
+            {showTaskForm && (
+                <TaskForm taskData={task} btnText="Save" handleSubmit={createTask}/>
+            )}
         </Container>
     )
 }
---
+++ src/components/task/TaskForm.js
@@ -0,0 +1,117 @@
+import { Button, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+import styles from './TaskForm.module.css'
+
+function TaskForm( {handleSubmit, btnText, taskData} ) {
+
+    const [task, setTask] = useState(taskData || {})
+    const [categories, setCategories] = useState([])
+    const [description, setDescription] = useState(task.description ? task.description : '')
+    const [selectedCategory, setSelectedCategory] = useState(task.category ? task.category.id : '')
+    const [selectedDate, setSelectedDate] = useState(task.due_date ? task.due_date : Date.now())
+
+    useEffect( () => {
+        fetch('http://localhost:5000/categories', {
+            method : 'GET',
+            headers : {
+                'Content-Type' : 'application/json'
+            }
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
+
+    const submit = (e) => {
+        e.preventDefault()
+        task.completed = false
+        if(!task.due_date) {
+            task.due_date = Date.now().toString()
+        }
+
+        handleSubmit(task)
+    }
+
+    function handleOnChange(e) {
+        setDescription(e.target.value)
+        setTask(
+            {
+                ...task,
+                description : e.target.value
+            }
+        )
+        
+    }
+
+    function handleSelectOnChange(e) {
+        setSelectedCategory(e.target.value)
+        setTask(
+            {
+                ...task,
+                category : e.target.value
+            }
+        )
+    }
+
+    function handleDateOnChange(date) {
+        setSelectedDate(date.toString());
+        setTask(
+            {
+                ...task,
+                due_date : date.toString()
+            }
+        )
+          
+    }
+
+    return (
+        <div className={styles.container}>
+            <Typography sx={{marginBottom : "0.5em"}} component="h2" variant="h5">
+                Task:
+            </Typography>
+            <form onSubmit={submit}>
+                <TextField 
+                    label="Task description" 
+                    sx={{width : "100%", marginBottom : "0.5em"}} 
+                    value={description ? description : ""} 
+                    onChange={handleOnChange}
+                />
+                <InputLabel id="select-category">Category</InputLabel>
+                <Select 
+                    sx={{width : "300px", marginBottom : "1em"}} 
+                    onChange={handleSelectOnChange} 
+                    value={selectedCategory} 
+                    labelId="select-category" 
+                    id="select-category" 
+                    label="Category"
+                >
+                    {categories.map( (cat) => (
+                        <MenuItem value={cat.id} key={cat.id}>{cat.name}</MenuItem>        
+                    ))}
+                </Select>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DatePicker
+                        label="Basic example"
+                        value={selectedDate}
+                        inputFormat="dd/MM/yyyy"
+                        onChange={handleDateOnChange}
+                        renderInput={(params) => <TextField {...params} />}
+                        
+                    />
+                </LocalizationProvider>
+                <Button variant="contained" type="submit" sx={{marginTop : "1em", width: "70px"}} >
+                    {btnText}
+                </Button>
+            </form>
+            
+        </div>
+    )
+}
+
+export default TaskForm
